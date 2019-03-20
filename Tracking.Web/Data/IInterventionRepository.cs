f6d5b18b8526178052f0dbc257bdf20d3427e0a3
Dmitriy Borowskiy <d.borowskij@gmail.com>
2019-03-20 12:18:56 +0300

    intervention repository

---
+++ Tracking.Web/Data/IInterventionRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tracking.Web.Models;
+
+namespace Tracking.Web.Data
+{
+    public class IInterventionRepository
+    {
+        IQueryable<Intervention> Interventions { get; }
+    }
+}
---
+++ Tracking.Web/Data/InterventionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tracking.Web.Data;
+
+namespace Tracking.Web.Data
+{
+    public class InterventionRepository : IInterventionRepository
+    {
+        private ApplicationDbContext _context;
+
+        public InterventionRepository(ApplicationDbContext con)
+        {
+            _context = con;
+        }
+    }
+}
