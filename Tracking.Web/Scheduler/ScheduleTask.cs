1dc008f7cf2b198f7e8c3be68e29491efcfeebbd
Dmitriy Borowskiy <d.borowskij@gmail.com>
2019-04-30 10:37:43 +0300

    background schedule task import survey audit

---
+++ Tracking.Web/Scheduler/ScheduleTask.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Tracking.Web.Services;
+
+namespace Tracking.Web.Scheduler
+{
+    public class ScheduleTask : ScheduledProcessor
+    {
+        private readonly IImportExportService _importExportService;
+        public ScheduleTask(IServiceScopeFactory serviceScopeFactory, IImportExportService importExportService) 
+            : base(serviceScopeFactory)
+        {
+            _importExportService = importExportService;
+        }
+
+        protected override string Schedule => "*/5****"; // runs every 5 minutes
+        public override Task ProcessInScope(IServiceProvider serviceProvider)
+        {
+            _importExportService.ImportSurveysAudit();
+            return Task.CompletedTask;
+        }
+    }
+}
